1be21e3b79edc55ce78eefdc241d800520921cbf
Bark Sanson <barksansondev@gmail.com>
2024-08-20 23:28:48 +0200

    Better Wofi

--- home-manager/wofi/wofi.nix
+++ home-manager/wofi/wofi.nix
@@ -2,5 +2,12 @@
     programs.wofi = {
         enable = true;
         style = (builtins.readFile ./style.css);
+        settings = {
+            prompt = "";
+            hide_scroll = true;
+            allow_images = true;
+            allow_markup = true;
+            lines = 8;
+        };
     };
 }
