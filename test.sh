77fa6c61cb652ca94c1011e76cfcfe94fe67ce04
Bark Sanson <barksansondev@gmail.com>
2024-08-20 21:56:01 +0200

    Some changes to the scripts

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
-cp home-manager/waybar/theme.css /home/$(whoami)/.config/waybar
-cp home-manager/waybar/power_menu.xml /home/$(whoami)/.config/waybar
+if [ ! -d "$HOME/.config/waybar" ]; then
+    mkdir "$HOME/.config/waybar"
+fi
 
-cp home-manager/wofi/style.css /home/$(whoami)/.config/wofi
+cp home-manager/waybar/theme.css $HOME/.config/waybar
+cp home-manager/waybar/power_menu.xml $HOME/.config/waybar
+
+if [ ! -d "$HOME/.config/wofi" ]; then
+    mkdir "$HOME/.config/wofi"
+fi
+
+cp home-manager/wofi/style.css $HOME/.config/wofi
 
 sudo nixos-rebuild test --flake ./
 
