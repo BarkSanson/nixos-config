b38f36f0349747327fce0987b8c59a36807f3ff0
Bark Sanson <barksansondev@gmail.com>
2024-08-20 23:57:55 +0200

    Now I actually added Fastfetch

---
+++ home-manager/fastfetch.nix
@@ -0,0 +1,21 @@
+{
+    programs.fastfetch = {
+        enable = true;
+        settings = {
+            modules = [
+                "title"
+                "separator"
+                "os"
+                "kernel"
+                "uptime"
+                "shell"
+                "wm"
+                "terminal"
+                "cpu"
+                "memory"
+                "battery"
+                "colors"
+            ];
+        }
+    };
+}
