af1bf225df18817003900b4f0aa67fb7bc110404
Bark Sanson <barksansondev@gmail.com>
2024-08-21 13:34:42 +0200

    Lua and it's modules xd

---
+++ home-manager/neovim/lua/settings.lua
@@ -0,0 +1,12 @@
+vim.cmd[[
+  syntax enable
+]]
+
+" Numbers and relative numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+" Don't use tabs, use spaces always
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 2
