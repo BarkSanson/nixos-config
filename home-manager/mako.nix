30dc1e18f0235174c3b7a68b38431ed541eb6a8b
Bark Sanson <barksansondev@gmail.com>
2024-08-22 19:28:39 +0200

    Configured Mako

--- home-manager/mako.nix
+++ home-manager/mako.nix
@@ -1,5 +1,14 @@
 {
-  programs.mako = {
+  services.mako = {
     enable = true;
+
+    borderRadius = 10;
+    borderColor = "#f4dfba";
+
+    textColor = "#f4dfba";
+
+    backgroundColor = "#876445";
+
+    defaultTimeout = 6500;
   };
 }
